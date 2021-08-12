4bad25945f2a532123c4e726212b4999ac794154
NajibAbdul12 <najibabdulkhadir@hotmail.com>
2021-08-12 13:46:04 +0100

    changes made

--- LoyaltyCardScheme2/Models/Address.cs
+++ LoyaltyCardScheme2/Models/Address.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 namespace LoyaltyCardScheme2.Models
 {
 	public class Address
-	{
-		public string Address1 { get; set; }
-		public string Address2 { get; set; }
+    {
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
 		public string Address3 { get; set; }
 		public string Address4 { get; set; }
+		public string Postcode { get; set; }
 	}
-
 }
--- LoyaltyCardScheme2/Models/Customer.cs
+++ LoyaltyCardScheme2/Models/Customer.cs
@@ -6,15 +6,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LoyaltyCardScheme2.Models
 {
-    public class Customer : Address
+    public class Customer 
     {
-
         [Key]
         public int ID { get; set; }
         public string Title { get; set; }
         public string FirstName { get; set; }
         public string Surname { get; set; }
-        public string PostCode { get; set; }
+        public Address Address { get; set; }
         public long Telephone { get; set; }
         public int Age { get; set; }
     }
